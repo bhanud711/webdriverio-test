002e4d8a7f2df36d9b9dde187932b279b152031d
Ramarao <bhanud7111@gmail.com>
2021-03-27 16:12:42 +0530

    webdriverIo

---
+++ tests/specs/search.js
@@ -0,0 +1,15 @@
+describe('All Testing Website', ()=>{
+    it('should open the flipkart url', ()=>{
+        browser.url('https://www.testandquiz.com/selenium/testing.html');
+        expect(browser).toHaveTitle('Sample Test Page');
+    })
+
+    it('provide values', ()=>{
+        const firstName=$('#fname');
+        const button=$('idOfButton');
+        firstName.addvalue('rama');
+        button.click();
+
+    })
+
+    })
